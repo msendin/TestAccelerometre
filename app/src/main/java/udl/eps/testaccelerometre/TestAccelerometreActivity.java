b09908818d8ead55a7ddb8394aae47ac0cef63fd
Montse <msendinv@gmail.com>
2022-02-08 14:34:36 +0100

    Upgraded to the 30 API level version

---
+++ app/src/main/java/udl/eps/testaccelerometre/TestAccelerometreActivity.java
@@ -0,0 +1,83 @@
+package udl.eps.testaccelerometre;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
+
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class TestAccelerometreActivity extends Activity implements SensorEventListener {
+  private SensorManager sensorManager;
+  private boolean color = false;
+  private TextView view;
+  private long lastUpdate;
+
+  
+/** Called when the activity is first created. */
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+    view = findViewById(R.id.textView);
+    view.setBackgroundColor(Color.GREEN);
+
+    sensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+    //if (sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER) != null) {
+      sensorManager.registerListener(this,
+              sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER),
+              SensorManager.SENSOR_DELAY_NORMAL);
+      // register this class as a listener for the accelerometer sensor
+    //}
+      lastUpdate = System.currentTimeMillis();
+  }
+
+  @Override
+  public void onSensorChanged(SensorEvent event) {
+          getAccelerometer(event);
+  }
+
+  private void getAccelerometer(SensorEvent event) {
+    float[] values = event.values;
+    // Movement
+    float x = values[0];
+    float y = values[1];
+    float z = values[2];
+
+    float accelationSquareRoot = (x * x + y * y + z * z)
+        / (SensorManager.GRAVITY_EARTH * SensorManager.GRAVITY_EARTH);
+    long actualTime = System.currentTimeMillis();
+    if (accelationSquareRoot >= 2) 
+    {
+      if (actualTime - lastUpdate < 200) {
+        return;
+      }
+      lastUpdate = actualTime;
+
+      Toast.makeText(this, R.string.shuffed, Toast.LENGTH_SHORT).show();
+      if (color) {
+        view.setBackgroundColor(Color.GREEN);
+      } else {
+        view.setBackgroundColor(Color.RED);
+      }
+      color = !color;
+    }
+  }
+
+  @Override
+  public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    // Do something here if sensor accuracy changes.
+  }
+
+  @Override
+  protected void onPause() {
+    // unregister listener
+    super.onPause();
+    sensorManager.unregisterListener(this);
+  }
+} 
